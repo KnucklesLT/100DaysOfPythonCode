2642a813b1ec16993934593f9494fa3de2186254
Luigi T <luigitejada@yahoo.com>
2023-02-09 14:42:10 -0500

    edit type variables

--- Day2.py
+++ Day2.py
@@ -1,6 +1,6 @@
 two_digit_number = input("Type a two digit number: ")
 
-first_num = int(str(two_digit_number)[0])
-second_num = int(str(two_digit_number)[1])
+first_num = int(two_digit_number[0])
+second_num = int(two_digit_number[1])
 
 print(first_num + second_num)
