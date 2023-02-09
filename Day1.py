1bd1b9b22a646ee3d3a9c61d473eec7b34065bbd
Luigi T <luigitejada@yahoo.com>
2023-02-09 12:34:08 -0500

    variables

---
+++ Day1.py
@@ -0,0 +1,14 @@
+print("Hello world!")
+
+name = input("What is your name? ")
+print(len(name))
+
+a = input("a: ")
+b = input("b: ")
+
+temp = a
+a = b
+b = temp
+
+print("a: " + a)
+print("b: " + b)
