4effc002b3bbe89889029100ad0e6d770c09802c
Luigi T <luigitejada@yahoo.com>
2023-02-09 20:04:04 -0500

    bmi calculator

--- Day2.py
+++ Day2.py
@@ -4,3 +4,9 @@ first_num = int(two_digit_number[0])
 second_num = int(two_digit_number[1])
 
 print(first_num + second_num)
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+bmi = int(weight) // float(height)**2
+print(int(bmi))
