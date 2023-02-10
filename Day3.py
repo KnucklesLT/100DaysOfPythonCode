23e8a3989a65c348adc08afccf3f5a5b2d27a6d4
Luigi T <luigitejada@yahoo.com>
2023-02-10 14:30:02 -0500

    pizza calculator

--- Day3.py
+++ Day3.py
@@ -35,3 +35,35 @@ if (year % 4 == 0):
         print("Leap year")
 else:
     print("Not leap year")
+
+
+#pizza calculator
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+bill = 0
+
+if size.lower() == 's':
+    bill += 15
+    if add_pepperoni.lower() == 'y':
+        bill += 2
+    if extra_cheese.lower() == 'y':
+        bill += 1
+elif size.lower() == 'm':
+    bill += 20
+    if add_pepperoni.lower() == 'y':
+        bill += 3
+    if extra_cheese.lower() == 'y':
+        bill += 1
+elif size.lower() == 'l':   
+    bill += 25
+    if add_pepperoni.lower() == 'y':
+        bill += 3
+    if extra_cheese.lower() == 'y':
+        bill += 1
+else:
+    print("Pizza size incorrectly entered.")
+
+print(f"Your final bill is: ${bill}.")
