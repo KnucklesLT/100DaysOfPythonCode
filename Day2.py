91818ed6da2a336e3ee487f9bfa291ca8cd89806
Luigi T <luigitejada@yahoo.com>
2023-02-09 20:17:03 -0500

    days weeks and months left calculator

--- Day2.py
+++ Day2.py
@@ -10,3 +10,13 @@ weight = input("enter your weight in kg: ")
 
 bmi = int(weight) // float(height)**2
 print(int(bmi))
+
+
+age = input("What is your current age? ")
+age_left = 90 - int(age) 
+
+days_left = age_left * 365
+weeks_left = age_left * 52
+months_left = age_left * 12
+
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
