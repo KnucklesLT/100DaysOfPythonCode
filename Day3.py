babdfcc50afdb8c89b5fe0852561f9d06e4ad937
Luigi T <luigitejada@yahoo.com>
2023-02-10 14:13:02 -0500

    Leap year checker

--- Day3.py
+++ Day3.py
@@ -21,3 +21,17 @@ elif bmi > 30 and bmi < 35:
     print(f"Your BMI is {bmi}, you are obese.")
 else:
     print(f"Your BMI is {bmi}, you are clinically obese.")
+
+
+# Leap year checker
+year = int(input("Which year do you want to check? "))
+
+if (year % 4 == 0):
+    if (year % 400 == 0):
+        print("Leap year.")
+    elif (year % 100 == 0):
+        print("Not leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not leap year")
