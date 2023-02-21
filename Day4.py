a923ec0ab0735ab45f3279876bfb67bf07f23a83
Luigi T <luigitejada@yahoo.com>
2023-02-21 11:36:25 -0500

    rock, paper, scissors

--- Day4.py
+++ Day4.py
@@ -36,3 +36,48 @@ map[int(position[1])-1][int(position[0])-1] = 'X'
 
 print(f"{row1}\n{row2}\n{row3}")
 
+
+# rock, paper, scissors
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+player_choice = int(input("What do you choose?  Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+choices = [rock, paper, scissors]
+
+computer_choice = random.randint(0,len(choices)-1)
+
+print(choices[player_choice])
+
+print(f"Computer chose:\n{choices[computer_choice]}")
+
+if player_choice == computer_choice:
+    print("Draw")
+elif (player_choice == 0 and computer_choice == 2) or (player_choice == 1 and computer_choice == 0) or (player_choice == 2 and computer_choice == 1):
+    print("You Won!")
+else:
+    print("You lose")
